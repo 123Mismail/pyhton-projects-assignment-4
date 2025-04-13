c5e54cdcfecf6736a1a2e3f74d9ec7a556838a0c
123Mismail <musafirbaltistani786@gmail.com>
2025-04-13 09:03:19 +0500

    first commit

---
+++ 00pyton-intro/02_agreement_bot/main.py
@@ -0,0 +1,11 @@
+def main():
+    print("Hello and welcome!")
+    # taking favourite animal name from user
+    user_favourite_animal:str=input("Enter your favourite animal name :")
+    # showing the output 
+    print(f"What's your favorite animal? {user_favourite_animal}")
+    print(f"My favorite animal is also {user_favourite_animal}")
+
+
+if __name__ == "__main__":
+    main()
