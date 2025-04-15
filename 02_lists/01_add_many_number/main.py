1d651028777c94ee33b327b8d907c88884b92e3e
123Mismail <musafirbaltistani786@gmail.com>
2025-04-15 12:07:45 +0500

    first commit

--- 02_lists/01_add_many_number/main.py
+++ 02_lists/01_add_many_number/main.py
@@ -1,5 +1,24 @@
+
+def add_lis_evens_numb(number:list[int],mode:str="all")->int:
+    """
+    Add   elements of a list on the basis of even , odd, all
+    """
+    initial_sum:int=0
+    for num in number :
+        if mode=="all":
+            initial_sum += num
+        elif mode =="even":
+            if num%2 == 0:
+                initial_sum += num
+        elif mode =="odd":
+            if num%2 !=0:
+                initial_sum += num
+    return initial_sum
+
 def main():
-    print("Hello from 01-add-many-number!")
+    my_list:list[int]=[2,3,4,5,6]
+    sum=add_lis_evens_numb(my_list,"all")
+    print(f"Sum of  {my_list} element  is = {sum}")
 
 
 if __name__ == "__main__":
